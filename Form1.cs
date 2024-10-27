40d3b9c88c05dfa5012056a6955b092c8ada3fa5
Juan Marin <juanpablomarin766@gmail.com>
2024-10-27 14:59:19 -0500

    Primer commit

---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo_final
+{
+    public partial class UserControl1 : Form
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void botonPersonas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrarPersona_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminarPersona_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonObjetos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrarPrestamo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrarMaterial_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrarDevolucion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIncreCantiObjetos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonHistorial_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelFormularioHijo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panelInferior_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
